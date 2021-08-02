dfdc533ad2f7262c82233d065068d1f1f63f731c
Roland Kim Andre Solon <>
2021-08-03 04:34:48 +0800

    Changes

--- src/App.js
+++ src/App.js
@@ -1,55 +1,56 @@
-import logo from './logo.svg';
-import React, { Component } from 'react'
-import './App.css';
-import { Layout, Menu } from 'antd';
-
-const { Header, Content, Footer } = Layout;
+import logo from "./logo.svg";
+import React, { Component } from "react";
+import "./App.css";
+import { Layout, Menu } from "antd";
 import {
   ApolloClient,
   InMemoryCache,
   ApolloProvider,
   useQuery,
-  gql
+  gql,
 } from "@apollo/client";
 
-import 'antd/dist/antd.css';
+import "antd/dist/antd.css";
+
+const { Header, Content, Footer } = Layout;
 
 const client = new ApolloClient({
-  uri: 'https://rickandmortyapi.com/graphql',
-  cache: new InMemoryCache()
+  uri: "https://rickandmortyapi.com/graphql",
+  cache: new InMemoryCache(),
 });
 
 class App extends Component {
   constructor(props) {
-    super(props)
-    this.state = {}
+    super(props);
+    this.state = {
+      error: null,
+      isFetching: false,
+      data: [],
+    };
   }
 
   componentDidMount() {
-    console.log('whoop, whoop.')
+    console.log("whoop, whoop.");
   }
 
+  async Fetch(){
+    
+  }
 
   render() {
-    return (<div className="App">
-      <Layout className="layout">
-        <Header title="Atlas">
-          <div className="logo"></div>
-          <Menu theme="dark" mode="horizontal">
-            Rick n' Morty Info
-          </Menu>
-        </Header>
-
-
-      </Layout>
-    </div>)
+    return (
+      <div className="App">
+        <Layout className="layout">
+          <Header title="Atlas">
+            <div className="logo"></div>
+            <Menu theme="dark" mode="horizontal">
+              Rick n' Morty Info
+            </Menu>
+          </Header>
+        </Layout>
+      </div>
+    );
   }
 }
-
-// function App() {
-//   return (
-
-//   );
-// }
 
 export default App;
